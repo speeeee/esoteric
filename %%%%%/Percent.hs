25c5727e59ad824bc83ca1908380f447c60595ff
Spencer <snsallay@gmail.com>
2015-11-10 18:12:04 -0500

    change stage

--- %%%%%/Percent.hs
+++ %%%%%/Percent.hs
@@ -6,7 +6,7 @@ import Control.Concurrent (threadDelay)
 import Data.Bits ( (.|.) )
 import System.Exit (exitWith, ExitCode(..))
 
-drawRect x y w h = mapM_ (\(x,y) -> glVertex3f x y 0) [(x,y),(x+w,y),(x+w,y+h),(x,y+h)]
+import Util.Assets
 
 initGL win = do
   glShadeModel gl_SMOOTH
@@ -21,20 +21,22 @@ resizeScene win w h = do
   glOrtho (-30) 30 (-30) 30 (-30) 30
   glMatrixMode gl_MODELVIEW
 
-drawScene _ = do
+drawScene (x,y,s) _ = do
   glClear $ fromIntegral $ gl_COLOR_BUFFER_BIT .|. gl_DEPTH_BUFFER_BIT
   glLoadIdentity
   glTranslatef (-1.0675) (-0.625) 0
-  glColor3f 1.0 0.5 0.5
+  glTranslatef (x*s) (y*s) 0
+  {-glColor3f 1.0 0.5 0.5
   glBegin gl_POLYGON
-  mapM_ (\(x,y) -> glVertex3f x y 0) [(0.4,0),(0.6,0),(1,0.4),(1,0.6),
-                                      (0.6,1),(0.4,1),(0,0.6),(0,0.4)]
+  mapM_ (\(x,y) -> glVertex3f (s*x) (s*y) 0) [(0.4,0),(0.6,0),(1,0.4),(1,0.6),
+                                              (0.6,1),(0.4,1),(0,0.6),(0,0.4)]
   glEnd
   glBegin gl_QUADS
   glColor3f 1 1 1
-  drawRect 0.2 0.45 0.6 0.1
-  drawRect 0.45 0.2 0.1 0.6
-  glEnd
+  drawRect (s*0.2) (s*0.45) (s*0.6) (s*0.1)
+  drawRect (s*0.45) (s*0.2) (s*0.1) (s*0.6)
+  glEnd-}
+  opener 0 0 s
 
 shutdown :: K.Window -> IO ()
 shutdown win = do
@@ -48,32 +50,43 @@ isPressed K.KeyState'Pressed = True
 isPressed K.KeyState'Repeating = True
 isPressed _ = False
 
-getInput :: K.Window -> IO (GLfloat, GLfloat)
+getInput :: K.Window -> IO (GLfloat, GLfloat,GLfloat)
 getInput win = do
   x0 <- isPressed `fmap` K.getKey win K.Key'Left
   x1 <- isPressed `fmap` K.getKey win K.Key'Right
   y0 <- isPressed `fmap` K.getKey win K.Key'Down
   y1 <- isPressed `fmap` K.getKey win K.Key'Up
+  s0 <- isPressed `fmap` K.getKey win K.Key'I
+  s1 <- isPressed `fmap` K.getKey win K.Key'K
   let x0n = if x0 then -1 else 0
       x1n = if x1 then 1 else 0
       y0n = if y0 then -1 else 0
       y1n = if y1 then 1 else 0
-  return (x0n + x1n, y0n + y1n)
+      s0n = if s0 then 0.9 else 1
+      s1n = if s1 then 1.1 else 1
+  return (x0n + x1n, y0n + y1n, s0n * s1n)
 
-runGame win = runGame' win (0::Int)
-runGame' win acc = do
+parseInput :: (GLfloat,GLfloat,GLfloat) -> K.Window -> IO (GLfloat,GLfloat,GLfloat)
+parseInput (x,y,s) win = do
+  (x',y',s') <- liftIO $ getInput win
+  return (x+x'*0.5/s,y+y'*0.5/s,s*s')
+
+--runGame win = runGame' win (0::Int)
+runGame nl win = do
+  nl' <- parseInput nl win
   K.pollEvents
-  drawScene win
+  drawScene nl' win
   K.swapBuffers win
-  runGame' win (acc + 1)
+  runGame nl' win
 
 main = do
   True <- K.init
   Just win <- K.createWindow 800 800 "%%%%%" Nothing Nothing
+  let nl = (0,0,1)
   K.makeContextCurrent (Just win)
-  K.setWindowRefreshCallback win (Just drawScene)
+  K.setWindowRefreshCallback win (Just (drawScene nl))
   K.setFramebufferSizeCallback win (Just resizeScene)
   K.setWindowCloseCallback win (Just shutdown)
   initGL win
-  runGame win
+  runGame nl win
 
