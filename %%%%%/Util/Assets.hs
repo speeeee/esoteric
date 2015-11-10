674a87ab3589d3d5327ff6890bb1bc339f1540e8
Spencer <snsallay@gmail.com>
2015-11-10 18:11:46 -0500

    add assets file

---
+++ %%%%%/Util/Assets.hs
@@ -0,0 +1,17 @@
+module Util.Assets (opener) where
+
+import Graphics.Rendering.OpenGL.Raw
+
+drawRect x y w h = mapM_ (\(x,y) -> glVertex3f x y 0) [(x,y),(x+w,y),(x+w,y+h),(x,y+h)]
+
+opener x y s = do
+  glBegin gl_POLYGON
+  glColor3f 1.0 0.5 0.5
+  mapM_ (\(x,y) -> glVertex3f (s*x) (s*y) 0) [(0.4,0),(0.6,0),(1,0.4),(1,0.6),
+                                              (0.6,1),(0.4,1),(0,0.6),(0,0.4)]
+  glEnd
+  glBegin gl_QUADS
+  glColor3f 1 1 1
+  drawRect (s*0.2) (s*0.45) (s*0.6) (s*0.1)
+  drawRect (s*0.45) (s*0.2) (s*0.1) (s*0.6)
+  glEnd
