908535e6c221e9d501f623d025b31f30fbab0b11
Spencer <snsallay@gmail.com>
2015-11-11 19:23:13 -0500

    fix error

--- %%%%%/Util/Assets.hs
+++ %%%%%/Util/Assets.hs
@@ -7,11 +7,11 @@ drawRect x y w h = mapM_ (\(x,y) -> glVertex3f x y 0) [(x,y),(x+w,y),(x+w,y+h),(
 opener x y s = do
   glBegin gl_POLYGON
   glColor3f 1.0 0.5 0.5
-  mapM_ (\(x,y) -> glVertex3f (s*x) (s*y) 0) [(0.4,0),(0.6,0),(1,0.4),(1,0.6),
-                                              (0.6,1),(0.4,1),(0,0.6),(0,0.4)]
+  mapM_ (\(xx,yy) -> glVertex3f (s*(x+xx)) (s*(y+yy)) 0) [(0.4,0),(0.6,0),(1,0.4),(1,0.6),
+                                                      (0.6,1),(0.4,1),(0,0.6),(0,0.4)]
   glEnd
   glBegin gl_QUADS
   glColor3f 1 1 1
-  drawRect (s*0.2) (s*0.45) (s*0.6) (s*0.1)
-  drawRect (s*0.45) (s*0.2) (s*0.1) (s*0.6)
+  drawRect (s*(x+0.2)) (s*(y+0.45)) (s*0.6) (s*0.1)
+  drawRect (s*(x+0.45)) (s*(y+0.2)) (s*0.1) (s*0.6)
   glEnd
