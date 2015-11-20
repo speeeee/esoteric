d8b9286e1a5ca6d28cf9658b548d8856acf94eda
Spencer <snsallay@gmail.com>
2015-11-19 21:38:53 -0500

    change Code type

--- GOLFLANG/Core/CodeFrame.hs
+++ GOLFLANG/Core/CodeFrame.hs
@@ -1,9 +1,10 @@
-module Core.CodeFrame (Mode(..),CF(..)) where
+module Core.CodeFrame (Mode(..),CF(..),Code(..)) where
 
 import Graphics.Rendering.OpenGL.Raw
 
 data Mode = WordSelect | Neutral deriving (Show,Eq)
 data CF = CF { mode :: Mode,
+               pos  :: Int,
                code :: Code }
 
-data Code = FunCall String Code | Lit GLfloat deriving (Show,Eq)
+data Code = FunCall String Code | List [Code] | Lit GLfloat | Useless deriving (Show,Eq)
