f61aa0a7c9fba4c2899f67f7cd3e242a4728c7b9
Spencer <snsallay@gmail.com>
2015-11-20 20:05:14 -0500

    remove color restriction

--- GOLFLANG/Util/Font.hs
+++ GOLFLANG/Util/Font.hs
@@ -11,7 +11,7 @@ data Dir = DownLeft | DownRight | UpRight | UpLeft
 -- tinySquare: it is as its name suggests. (0.2×0.2)
 tinySquare :: (GLfloat,GLfloat) -> GLfloat -> IO ()
 tinySquare (x,y) s = do
-  glColor3f 1 1 1
+  --glColor3f 1 1 1
   glBegin gl_QUADS
   mapM_ (\(x',y') -> glVertex3f (x+x'*s) (y+y'*s) 0) [(0,0),(1,0),(1,1),(0,1)]
   glEnd
@@ -20,7 +20,7 @@ tinySquare (x,y) s = do
 switchPath :: (GLfloat,GLfloat) -> Dir -> GLfloat -> IO ()
 switchPath (x,y) dest s = do
   let lst = map ((*) (pi/2)) [0.05,0.1..0.95]
-  glColor3f 1 1 1
+  --glColor3f 1 1 1
   glBegin gl_POLYGON
   mapM_ (\(x',y') -> glVertex3f (x+x') (y+y') 0)
     (case dest of DownLeft -> [(0,s),(s,s)] ++ (map (\k -> ((s*cos k),(-s*sin k+s))) lst) ++ [(0,0)]
