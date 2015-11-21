a92f4a03a6616fc0739c4db748dcd7694bc82217
Spencer <snsallay@gmail.com>
2015-11-20 20:04:53 -0500

    add rendering for a useless code

--- GOLFLANG/Core/CodeFrame.hs
+++ GOLFLANG/Core/CodeFrame.hs
@@ -1,6 +1,7 @@
-module Core.CodeFrame (Mode(..),CF(..),Code(..)) where
+module Core.CodeFrame (Mode(..),CF(..),Code(..),drawCode) where
 
 import Graphics.Rendering.OpenGL.Raw
+import Util.Font
 
 data Mode = WordSelect | Neutral deriving (Show,Eq)
 data CF = CF { mode :: Mode,
@@ -8,3 +9,7 @@ data CF = CF { mode :: Mode,
                code :: Code }
 
 data Code = FunCall String Code | List [Code] | Lit GLfloat | Useless deriving (Show,Eq)
+
+drawCode :: Code -> IO ()
+drawCode Useless = do
+  drawString (-28.75,27.75) "useless." 0.25
