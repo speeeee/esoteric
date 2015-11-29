b5a9209bc69568583a2f1c4ef9e0ae09350df82c
Spencer <snsallay@gmail.com>
2015-11-28 23:30:43 -0500

    add ball shape

--- GOLFLANG/Util/Shapes.hs
+++ GOLFLANG/Util/Shapes.hs
@@ -1,4 +1,4 @@
-module Util.Shapes (rect) where
+module Util.Shapes (rect,ball) where
 
 import Graphics.Rendering.OpenGL.Raw
 
@@ -7,14 +7,20 @@ rect x y w h = do
   mapM_ (\(x',y') -> glVertex3f (x+x') (y+y') 0) [(0,0),(w,0),(w,h),(0,h)]
   glEnd
 
-cover (x,y,w,h) (r,g,b) wid = do
+ball x y r = do
+  glBegin gl_POLYGON
+  mapM_ (\(t) -> glVertex3f (x+r*cos t) (y+r*sin t) 0) $ map ((*) (2*pi)) [0,0.05..1]
+  glEnd
+
+{-cover (x,y,w,h) (r,g,b) wid = do
   glBegin gl_QUADS
   glColor3f r g b
   rect x y w h
   glColor3f (r-0.1) (g-0.1) (b-0.1)
+  --rect (x+5) (y+5) w h
   mapM_ (\(x',y') -> glVertex3f (x+x') (y+y') 0) [(0,0),(w*wid,h*wid),(w-w*wid,h*wid),(w,0),
                                                   (w,0),(w-w*wid,h*wid),(w-w*wid,h-h*wid),(w,h)]
   glColor3f (r-0.2) (g-0.2) (b-0.2)
   mapM_ (\(x',y') -> glVertex3f (x+x') (y+y') 0) [(0,0),(w*wid,h*wid),(w*wid,h-h*wid),(0,h),
                                                   (0,h),(w*wid,h-h*wid),(w-w*wid,h-h*wid),(w,h)]
-  glEnd
+  glEnd-}
