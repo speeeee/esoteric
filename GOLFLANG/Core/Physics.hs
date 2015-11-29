46a5b1572b8e9b0002b2f434d18a40290062f5df
Spencer <snsallay@gmail.com>
2015-11-28 23:31:02 -0500

    add various attributes for the ball

--- GOLFLANG/Core/Physics.hs
+++ GOLFLANG/Core/Physics.hs
@@ -1,9 +1,11 @@
-module Core.Physics (Ball(..),Course(..)) where
+module Core.Physics (Ball(..),Course(..),Board(..)) where
 
 import Graphics.Rendering.OpenGL.Raw
 
--- Ball (x,y) velocity theta height
-data Ball = Ball (GLfloat,GLfloat) GLfloat GLfloat GLfloat
--- Course Ball Stroke Par Map
-data Course = Course Ball Int Int [Int]
+-- Ball (x,y) velocity theta theta-on-y-axis height
+data Ball = Ball (GLfloat,GLfloat) GLfloat GLfloat GLfloat GLfloat (GLfloat -> GLfloat)
+-- Board Width Height Map
+data Board = Board Int Int [Int]
+-- Course Ball Stroke Par Board
+data Course = Course Ball Int Int Board
 
