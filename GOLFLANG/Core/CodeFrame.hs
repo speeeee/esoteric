de1ebf9ca734db1f223b4badf066bad6be7043f1
Spencer <snsallay@gmail.com>
2015-11-29 22:45:40 -0500

    add Transit

--- GOLFLANG/Core/CodeFrame.hs
+++ GOLFLANG/Core/CodeFrame.hs
@@ -3,7 +3,7 @@ module Core.CodeFrame (Mode(..),CF(..),Code(..),drawCode) where
 import Graphics.Rendering.OpenGL.Raw
 import Util.Font
 
-data Mode = WordSelect | Neutral Bool | VelSel Int Bool deriving (Show,Eq)
+data Mode = WordSelect | Neutral Bool | Transit Bool | VelSel Int Bool deriving (Show,Eq)
 data CF = CF { mode :: Mode,
                pos  :: Int,
                code :: Code }
