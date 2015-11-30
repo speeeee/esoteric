cf475fbf26ae123f351155dfc9c52d525075d40a
Spencer <snsallay@gmail.com>
2015-11-29 22:22:10 -0500

    add button

--- GOLFLANG/Util/Shapes.hs
+++ GOLFLANG/Util/Shapes.hs
@@ -1,6 +1,7 @@
-module Util.Shapes (rect,ball) where
+module Util.Shapes (rect,ball,button) where
 
 import Graphics.Rendering.OpenGL.Raw
+import Util.Font
 
 rect x y w h = do
   glBegin gl_QUADS
@@ -11,6 +12,14 @@ ball x y r = do
   glBegin gl_POLYGON
   mapM_ (\(t) -> glVertex3f (x+r*cos t) (y+r*sin t) 0) $ map ((*) (2*pi)) [0,0.05..1]
   glEnd
+
+button x y w h t s (r,g,b) = do
+  glBegin gl_QUADS
+  glColor3f r g b
+  rect x y w h
+  glEnd
+  glColor3f (r-0.2) (g-0.2) (b-0.2)
+  drawString (x+(w-(5.2*(fromIntegral $ length t)*s))/2,y+(h+5*s)/2) t s
 
 {-cover (x,y,w,h) (r,g,b) wid = do
   glBegin gl_QUADS
