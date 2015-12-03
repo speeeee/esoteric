10aa16308edb0dcdb8a7f7f7ba8ca75167e87f9a
Spencer <snsallay@gmail.com>
2015-12-02 20:19:40 -0500

    add origin

--- GOLFLANG/Core/Physics.hs
+++ GOLFLANG/Core/Physics.hs
@@ -2,8 +2,8 @@ module Core.Physics (Ball(..),Course(..),Board(..)) where
 
 import Graphics.Rendering.OpenGL.Raw
 
--- Ball (x,y) velocity theta theta-on-z-axis height time function
-data Ball = Ball (GLfloat,GLfloat) GLfloat GLfloat GLfloat GLfloat GLfloat (GLfloat -> GLfloat)
+-- Ball (x0,y0) (x,y) velocity theta theta-on-z-axis height time function
+data Ball = Ball (GLfloat,GLfloat) (GLfloat,GLfloat) GLfloat GLfloat GLfloat GLfloat GLfloat (GLfloat -> GLfloat)
 -- Board Width Height Map
 data Board = Board Int Int [Int]
 -- Course Ball Stroke Par Board
