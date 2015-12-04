bb8b5a22fe7a393dce9bfa9aee292298a6091616
Spencer <snsallay@gmail.com>
2015-12-03 21:33:08 -0500

    finalize fairway color

--- GOLFLANG/Util/Course.hs
+++ GOLFLANG/Util/Course.hs
@@ -4,7 +4,7 @@ import Graphics.Rendering.OpenGL.Raw
 import Foreign.C.Types
 import System.Random
 
-data CseElt = Fairway | Rough | UnRough | Bunker | SUPERFairway | Green  | Water | OB
+data CseElt = Fairway | Rough | UnRough | SUPERFairway | Green  | Water | OB
             | Hole | DL | DR | UL | UR deriving (Show,Eq)
 data CseArea = Block (GLfloat,GLfloat) GLfloat GLfloat CseElt
              | Func (GLfloat,GLfloat) (GLfloat -> GLfloat) (GLfloat -> GLfloat) GLfloat CseElt
@@ -18,8 +18,10 @@ baseboard = do
 
 drawBoard :: CseArea -> IO ()
 drawBoard (Block (x,y) w h Fairway) = do
-  glBegin gl_LINES
-  mapM_ (\k -> do glColor3f (sin (k*0.125)*0.1725+0.5) 0 (sin (k*0.125)*0.1725+0.5) -- possible: (0.5,0.25) (0.5,0.5)
-                  glVertex3f (x+k) y 0
-                  glVertex3f (x+k) (y+h) 0) [0,0.2..w]
+  glBegin gl_QUADS
+  mapM_ (\k -> do glColor3f (sin (k*0.125)*0.1725+0.35) 0 (sin (k*0.125)*0.1725+0.5) -- possible: (0.5,0.25) (0.5,0.5)
+                  glVertex3f (x+k-0.2) y 0
+                  glVertex3f (x+k-0.2) (y+h) 0
+                  glVertex3f (x+k+0.2) (y+h) 0
+                  glVertex3f (x+k+0.2) y 0) [0,0.4..w]
   glEnd
