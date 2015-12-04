15a49dac74b50ec5011f94f2221c9ea7a72d3efb
Spencer <snsallay@gmail.com>
2015-12-03 20:32:23 -0500

    change type of 'cse'

--- GOLFLANG/Core/Physics.hs
+++ GOLFLANG/Core/Physics.hs
@@ -1,11 +1,10 @@
-module Core.Physics (Ball(..),Course(..),Board(..)) where
+module Core.Physics (Ball(..),Course(..)) where
 
 import Graphics.Rendering.OpenGL.Raw
+import Util.Course
 
 -- Ball (x0,y0) (x,y) velocity theta theta-on-z-axis height time function
 data Ball = Ball (GLfloat,GLfloat) (GLfloat,GLfloat) GLfloat GLfloat GLfloat GLfloat GLfloat (GLfloat -> GLfloat)
--- Board Width Height Map
-data Board = Board Int Int [Int]
 -- Course Ball Stroke Par Board
-data Course = Course Ball Int Int Board
+data Course = Course Ball Int Int [CseArea]
 
