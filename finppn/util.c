d8cafaea91443fe896016aee0843f856ab6b7701
Spencer <snsallay@gmail.com>
2015-12-27 21:39:35 -0500

    todo: fix 'join' function

--- finppn/util.c
+++ finppn/util.c
@@ -3,7 +3,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdarg.h>
+
+typedef int (*predicate)(int);
 
 char *join(char *in, char *f, char *s) {
   char *n = malloc((strlen(in)+strlen(f)+strlen(s)+1)*sizeof(char));
@@ -12,12 +15,13 @@ char *join(char *in, char *f, char *s) {
   for(int i=0;i<strlen(s);i++) { n[i+strlen(in)+strlen(f)] = s[i]; }
   n[strlen(s)+strlen(in)+strlen(f)] = '\0'; return n; }
 
-char *tok(int (*pred)(int), char *n, int c) {
-  if(n) { free(n); } int q = 1; int sz = 10;
-  n = malloc(10*sizeof(char)); n[0] = c; 
+char *tok(predicate pred, int c) {
+  /*if(n) { free(n); }*/ int q = 1; int sz = 10;
+  char *n = malloc(10*sizeof(char)); n[0] = c; 
   while(pred(c)) {
     if(q+1>sz) { n = realloc(n,(sz+=10)*sizeof(char)); } 
-    n[(q++)-1] = c; c = getchar(); } n[q] = '\0'; ungetc(c,stdin); }
+    n[(q++)-1] = c; c = getchar(); } n[q] = '\0'; ungetc(c,stdin); return n; }
 
-int num(int c) { isdigit(c)||c == '.'; }
-int str(int c) { c != '"'; } int sym(int c) { c!=' '&&c!='\t'&&c!='\n'&&c!=EOF; }
+int num(int c) { puts("num"); return isdigit(c)||c == '.'; }
+int str(int c) { puts("str"); return c != '"'; } 
+int sym(int c) { puts("sym"); return isgraph(c)&&!isdigit(c)&&c!='.'; }
--- finppn/util.h
+++ finppn/util.h
@@ -4,8 +4,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
+#include <ctype.h>
+
+typedef int (*predicate)(int);
 
 char *join(char *, char *, char *);
-char *tok(int *(int), char *, int);
+char *tok(predicate, int);
 
 int num(int); int str(int); int sym(int);
