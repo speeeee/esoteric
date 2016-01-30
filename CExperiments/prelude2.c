179b9bf6bf12709f6aff1717b0d28aea68b2205b
Spencer <snsallay@gmail.com>
2016-01-29 23:48:24 -0500

    fix segfault error

--- CExperiments/prelude2.c
+++ CExperiments/prelude2.c
@@ -21,6 +21,7 @@ typedef struct Lit Lit;
 typedef Lit (*FPtr)(Elem *);
 
 struct Lit { union { Int i; Flt f; Chr c; Elem *e; } x; unsigned int type; };
+//struct Lit { Int i; Flt f; Chr c; Elem *e; unsigned int type; };
 struct Elem { Lit lx; struct Elem *next; };
 typedef struct { int t; union { char *la; FPtr f; }; } Fun;
 
@@ -29,30 +30,36 @@ typedef struct { int t; union { char *la; FPtr f; }; } Fun;
                  case FLT: l.x.f = *(double *) x; break;
                  case CHR: l.x.c = *(char *) x; break;
                  case LST: l.x.e = *(Elem **) x; break; } l.type = type; return l; }*/
-Lit lit(int type, int n, ...) { 
+/*Lit lit(int type, int n, ...) { 
   va_list vl; va_start(vl,n); Lit l; l.type = type;
   switch(type) { case INT: l.x.i = va_arg(vl,long); break;
                  case FLT: l.x.f = va_arg(vl,double); break;
                  case CHR: l.x.c = va_arg(vl,int); break;
                  case LST: l.x.e = va_arg(vl,Elem *); break;
-                 default: exit(0); } return l; }
+                 default: printf("nem"); } printf("%i", l.type); return l; }*/
+Lit liti(long i) { Lit l; l.x.i = i; l.type = 0; return l; }
 
-Lit readInt(void) { int i; scanf("%d",&i); return lit(i,INT); }
+Lit readInt(void) { int i; scanf("%d",&i); return liti(i); }
 Elem *list(int n, ...) { 
-  va_list vl; va_start(vl,n); Elem *x; Elem *curr;
+  va_list vl; va_start(vl,n); Elem *x = malloc(sizeof(Elem));
+  Elem *curr = malloc(sizeof(Elem));
   for(int i=0;i<n;i++) { Lit val = va_arg(vl,Lit);
-    if(i==0) { printf("%li is val\n",val.x.i); x->lx = val; curr = x; }
+    if(i==0) { x->lx = val; curr = x; }
     else { Elem *n; n->lx = val; curr->next = n; curr = n; } }
   va_end(vl); return x; }
-Lit printAtom(Elem *a) { printf("%i\n",a->lx.type);
-  switch(a->lx.type) { case INT: printf("%ld",a->lx.x.i); break;
+Elem *nlist(Lit x) { Elem *e; e->lx = x; return e; }
+// Elem *cons(Elem *a, Elem *l)
+Lit printAtom(Elem *a) {
+  switch(a->lx.type) { case INT: printf("%li",a->lx.x.i); break;
                        case FLT: printf("%g",a->lx.x.f); break;
                        case CHR: printf("%c",a->lx.x.c); break;
                        default: printf("error\n"); }
-  return lit(0,INT); }
+  return liti(0); }
 
 Lit call(FPtr x, Elem *a) {
   return x(a); }
 
 int main(int argc, char **argv) {
-  call(&printAtom,list(1,lit(INT,1,3))); return 0; }
+  call(&printAtom,list(1,liti(3)));
+  //Elem *root = malloc(sizeof(Elem)); root->lx.i = 3; root->lx.type = 0;
+  /*printAtom(root); printf("%li",root->lx.i);*/ return 0; }
