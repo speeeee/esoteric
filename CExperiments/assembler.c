8e7b88a2f111504f0fcb753e465b80fd0c6eb12a
Spencer <snsallay@gmail.com>
2016-02-11 21:53:39 -0500

    add byte-code translation for sample

--- CExperiments/assembler.c
+++ CExperiments/assembler.c
@@ -1,7 +1,7 @@
 /* bytecode
-   op-codes: 00 - push_byte (1 byte, chars)
-             01 - push_word (4 bytes, ints, pointers)
-             02 - push_dword (8 bytes, doubles, long ints)
+   op-codes: 00 - pushb (1 byte, chars)
+             01 - pushw (4 bytes, ints, pointers)
+             02 - pushd (8 bytes, doubles, long ints)
              03 - malloc (pushes address to stack) (4 bytes)
              04 - realloc (4 bytes, 4 bytes)
              05 - free   (frees address at top of stack) (4 bytes)
@@ -16,6 +16,8 @@
              0E - jmp (LABEL or 4 bytes)
              0F - terminate
              10 - pop
+             11 - out_s
+             12 - in_s
 
    constants: TSTK - always points to the top of the stack. Note that referencing
                      this does not pop.
@@ -24,13 +26,16 @@
    label end
    terminate
    label main        ; labels are unnecessary, but good for gotos. 
-   push 32
-   push 33
-   push 34
+   pushw 32
+   pushw 33
+   pushw 34
    label loop
    jns end
-   out word TSTK
+   out_s
    pop
    jmp loop 
+  
+   or: 0B 00000000 0F 00000000 0B 00000001 01 00000020 01 00000021 01 00000022
+       0B 00000002 0D 00000000 09 00000000 10 00000000 0E 00000002
 */
    
