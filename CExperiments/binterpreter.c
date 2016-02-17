67e60b2062e63492c265ea9badca9b5dd16fec6f
Spencer <snsallay@gmail.com>
2016-02-16 21:22:44 -0500

    change general layout of parsing

--- CExperiments/binterpreter.c
+++ CExperiments/binterpreter.c
@@ -15,26 +15,30 @@
 #define MOV 8
 #define MOV_S 9
 #define CALL 10
-#define OUT 11
-#define IN 12
-#define LABEL 13
-#define REF 14
-#define JNS 15
-#define JMP 16
-#define TERM 17
-#define POP 18
-#define OUT_S 19
-#define IN_S 20
-#define MAIN 21
+#define CALL_S 11
+#define OUT 12
+#define IN 13
+#define LABEL 14
+#define REF 15
+#define REF_S 16
+#define JNS 17
+#define JNS_S 18
+#define JMP 19
+#define JMP_S 20
+#define TERM 21
+#define POP 22
+#define OUT_S 23
+#define IN_S 24
+#define MAIN 25
 
 typedef int    Word;
 typedef long   DWord;
 typedef double Flt;
 typedef char   Byte;
 
-typedef struct Lit Lit;
+/*typedef struct Lit Lit;
 struct Lit { union { Word i; DWord l; Flt f; Byte c; Byte *s; void *v; } x;
-             unsigned int type; };
+             unsigned int type; };*/
 //typedef struct Arr { 
 
 #define INT 0
@@ -49,75 +53,82 @@ struct Lit { union { Word i; DWord l; Flt f; Byte c; Byte *s; void *v; } x;
 #define L sizeof(long)
 #define F sizeof(double)
 
-typedef struct { int op; Lit x; } expr;
-expr *no_evals; int esz = 0;
 int *lbls; int lsz = 0;
 typedef struct Stk { void *x; struct Stk *prev; } Stk;
-Stk *stk;
+typedef struct { union { char c; int i; long l; double f;
+                         char *ca; int *ia; long *la; double *fa; }; } Lit;
+typedef struct { char op; Lit q; } Expr;
+Expr *exprs; int esz = 0; int mn = -1;
 
 void push_lbl(int plc) { lbls = realloc(lbls,(lsz+1)*sizeof(int));
   lbls[lsz++] = plc; }
-/*void push(void *x, int sz) { Lit l; l.type = sz;
-  switch(sz) { case INT: l.x.i = (int)x; break; case FLT: l.x.f = (double)x; break;
-               case CHR: l.x.c = (char)x; break; case LNG: l.x.l = (long)x; break;
-               case VOID: l.x.v = x; break; }
-  stk = realloc(stk,(ssz+1)*sizeof(Lit)); stk[ssz++] = l; }*/
-/*void push(void *x, int type) { Lit l; l.type = type;
-  switch(sz) { case INT: l.x.i = (int)x; break; case FLT: l.x.f = (double)x; break;
-               case CHR: l.x.c = (char)x; break; case LNG: l.x.l = (long)x; break;
-               case VOID: l.x.v = x; break; }*/
-void push(void *x) { if(stk->x) { Stk *q = malloc(sizeof(Stk));
+void push_expr(char op, Lit q) { exprs = realloc(exprs,(esz+1)*sizeof(Expr));
+  exprs[esz++] = (Expr) { op, q }; }
+/*void push(void *x) { if(stk->x) { Stk *q = malloc(sizeof(Stk));
   q->prev = malloc(sizeof(Stk)); q->prev = stk; stk = q; stk->x = x; }
   else { stk->x = x; } }
 int top_int(void) { int x = *(int *)stk->x; return x; }
 void pop(void) { if(stk->prev) { free(stk->x); stk = stk->prev; }
   else { printf("error: null stack.\n"); } }
 
-void DESTROY(void) { if(!stk->prev) { free(stk); } }
+void DESTROY(void) { if(!stk->prev) { free(stk); }
+  else { printf("%i",*(int *)stk->x); } }*/
 
-/*typedef struct { char *name; int argsz; } OpC;
-OpC opcodes[] = { { "pushw", I }, { "pushf", F }, { "pushc", B },
-                  { "pushl", L }, { "malloc", I }, 
-                  { "realloc", I }, { "free", 0 }, { "mov", I },
-                  { "mov_s", 0 }, { "call", I }, { "out", I },
-                  { "in", 0 }, { "label", I },
-                  { "ref", I }, { "jns", I }, { "jmp", I },
-                  { "terminate", 0 }, { "pop", 0 }, { "out_s", 0 },
-                  { "in_s", 0 } };*/
+int opcodes[] = { /*push*/INT,FLT,CHR,LNG,/*malloc*/0,/*realloc*/0,/*free*/0,
+                  /*mov*/INT,0,/*call*/INT,0,/*out*/INT,/*in*/0,/*label*/INT,
+                  /*ref*/INT,0,/*jns*/INT,0,/*jmp*/INT,0,/*terminate*/0,/*pop*/0,
+                  /*out_s*/0,/*in_s*/0,/*main*/0 };
 
-void read_prgm(FILE *f) { char op; int mn = 0;
+
+/*void read_prgm(FILE *f) { char op; int mn = 0;
   while((op = fgetc(f)) != TERM) { printf("%i",op); if(mn) { switch(op) {
-    case PW: { int x; fread(&x,I,1,f); push(&x); break; }
-    case PF: { double x; fread(&x,F,1,f); push(&x); break; }
-    case PC: { char x; fread(&x,B,1,f); push(&x); break; }
-    case PL: { long x; fread(&x,L,1,f); push(&x); break; }
+    case PW: { int *x = malloc(sizeof(int)); fread(x,I,1,f); push(x); break; }
+    case PF: { double *x = malloc(sizeof(double)); fread(x,F,1,f); push(x); break; }
+    case PC: { char *x = malloc(sizeof(char)); fread(x,B,1,f); push(x); break; }
+    case PL: { long *x = malloc(sizeof(long)); fread(x,L,1,f); push(x); break; }
     case MALLOC: { int x = top_int(); pop(); push(malloc(x)); break; } } }
     else if(op==MAIN) { mn = 1; }
     else if(op==LABEL) { int adr; fread(&adr,4,1,f);
-      push_lbl(adr); } } DESTROY(); }
+      push_lbl(adr); } } DESTROY(); }*/
 
-int main(int argc, char **argv) { stk = malloc(sizeof(Stk));
-  FILE *f; f = fopen("sample.usm","rb"); read_prgm(f); return 0; }
+void read_prgm(FILE *f) { char op;
+  while((op = fgetc(f)) != TERM) { switch(op) {
+    case LABEL: { int x; fread(&x,4,1,f); push_lbl(esz); break; }
+    case MAIN: { mn = esz; break; }
+    default: { Lit l; switch(opcodes[(int)op]) {
+      case CHR: { char i; fread(&i,1,1,f); l.c = i; break; }
+      case INT: { int i; fread(&i,4,1,f); l.i = i; break; }
+      case FLT: { double i; fread(&i,8,1,f); l.f = i; break; }
+      case LNG: { long i; fread(&i,8,1,f); l.l = i; break; } } push_expr(op,l); } } } }
 
-/*#define PW 0
+int main(int argc, char **argv) { //stk = malloc(sizeof(Stk));
+  exprs = malloc(sizeof(Expr));
+  FILE *f; f = fopen("sample.usm","rb"); read_prgm(f); free(exprs); return 0; }
+
+/*#define P 4
+#define PW 0
 #define PF 1
 #define PC 2
 #define PL 3
-#define MALLOC 4
-#define REALL 5
-#define FREE 6
-#define MOV 7
-#define MOV_S 8
-#define CALL 9
-#define OUT 10
-#define IN 11
-#define LABEL 12
-#define REF 13
-#define JNS 14
-#define JMP 15
-#define TERM 16
-#define POP 17
-#define OUT_S 18
-#define IN_S 19
-#define MAIN 20*/
+#define MALLOC 5
+#define REALL 6
+#define FREE 7
+#define MOV 8
+#define MOV_S 9
+#define CALL 10
+#define CALL_S 11
+#define OUT 12
+#define IN 13
+#define LABEL 14
+#define REF 15
+#define REF_S 16
+#define JNS 17
+#define JNS_S 18
+#define JMP 19
+#define JMP_S 20
+#define TERM 21
+#define POP 22
+#define OUT_S 23
+#define IN_S 24
+#define MAIN 25*/
 
