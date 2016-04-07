df0289fbe6d09b89dcd324c24cc03c48303469a9
Spencer <snsallay@gmail.com>
2016-04-06 22:14:25 -0400

    add example that generates wav file (currently adding the third sine wave changes the audio drastically)

---
+++ fraklang/fraklang.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+// WAV files only; assume data up to byte 44.
+
+void write_little_endian(unsigned int word, int num_bytes, FILE *wav_file) {
+    unsigned buf;
+    while(num_bytes>0) { buf = word & 0xff;
+      fwrite(&buf, 1,1, wav_file);
+      num_bytes--;
+    word >>= 8; } }
+
+void wav_header(FILE *f, int32_t sz, int16_t ch) {
+  fwrite("RIFF", 1, 4, f); write_little_endian(36+sz, 4, f);
+  fwrite("WAVE", 1, 4, f); fwrite("fmt ", 1, 4, f);
+  write_little_endian(16, 4, f);  write_little_endian(1, 2, f);  
+  write_little_endian(ch, 2, f); write_little_endian(44100, 4, f);
+  write_little_endian((16*44100*ch)/8, 4, f); write_little_endian((16*ch)/8, 2, f); 
+  write_little_endian(16, 2, f); 
+  fwrite("data", 1, 4, f); write_little_endian(sz, 4, f); }
+
+void out_buf(FILE *f, int16_t *buf, int32_t sz) {
+  for(int i=0;i<sz-44;i++) { write_little_endian(buf[i],2,f); } } 
+
+void sine_wave(int16_t *buf, int32_t len, float freq, float ratio, float amp) {
+  for(int i=0; i<len; i++) { float tht = ((float)i/ratio) * M_PI;
+    buf[i] += (int16_t)(sin(tht*freq)*32767.f*amp); } }
+
+int main(int argc, char **argv) { FILE *f;
+  int16_t buf[44100]; sine_wave(buf,44100,440,44100,0.5);
+  sine_wave(buf,44100,293.66,44100,0.5); //sine_wave(buf,44100,369.99,44100,0.5);
+  f = fopen("cdjoiw.wav","w"); wav_header(f,44100,1);
+  out_buf(f,buf,44100); return 0; }
