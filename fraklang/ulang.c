e6f87a3ad0c48b337f88581213db0e8cad385492
Spencer <snsallay@gmail.com>
2016-04-17 19:57:12 -0400

    fix nlstptr

--- fraklang/ulang.c
+++ fraklang/ulang.c
@@ -3,6 +3,7 @@
 #include <ctype.h>
 #include <string.h>
 
+#define NIL -1
 #define SYM 0
 #define INT 1
 #define FLT 2
@@ -31,27 +32,30 @@ Fun funs[] = { { "+", 2, NULL } }; int fsz = 1;
 
 Elem *top; Elem *stk;
 
+void nlstptr(Elem *s) { if(s->x.type!=NIL) {
+  s->next = malloc(sizeof(Elem)); s = s->next; } }
 void nlstptrg(void) { if(stk) { Elem *q = malloc(sizeof(Elem));
   stk->next = malloc(sizeof(Elem)); stk->next = q; stk = stk->next; } 
   else { top = stk = malloc(sizeof(Elem)); } }
-void appeg(Lit l) { nlstptrg(); stk->x = l; }
+void appeg(Lit l, Elem *s) { nlstptr(s); stk->x = l; }
 
 Lit liti(int64_t i) { Lit l; l.x.i = i; l.type = INT; return l; }
 Lit litsy(char *x) { Lit l; l.x.s = x; l.type = SYM; return l; }
 
-Lit tok(FILE *in) { Lit l; int c = fgetc(in); switch(c) {
+Lit tok(FILE *in) { Lit l; int c = fgetc(in); printf("%i\n",c); switch(c) {
   case I: fread(&l.x.i,sizeof(int64_t),1,in); l.type = INT; break;
   case F: fread(&l.x.f,sizeof(double),1,in); l.type = FLT; break;
   case S: { int e; fread(&e,sizeof(int64_t),1,in); fread(&l.x.s,1,e,in);
             l.type = SYM; break; }
-  case E: l.x.i = 0; l.type = EXP; case D: l.x.i = 0; l.type = FXP; break;
+  case E: l.x.i = 0; l.type = EXP; break; case D: l.x.i = 0; l.type = FXP; break;
   case EOF: l.x.i = 0; l.type = END; } return l; }
 
 void ureader(FILE *in, Elem *s) { Lit l; while((l=tok(in)).type!=END&&
                                                l.type!=FXP) {
-  if(l.type == EXP) { nlstptrg(); l = tok(in); s->x = l;
+  if(l.type == EXP) { nlstptr(s); l = tok(in); s->x = l;
     s->up = malloc(sizeof(Elem)); ureader(in,s->up); }
-  else { appeg(l); } } }
+  else { appeg(l,s); } } }
 
 int main(int argc, char **argv) { FILE *f; f = fopen("test.ul","rb");
+  stk = top = malloc(sizeof(Elem)); top->x.type = NIL;
   ureader(f,top); fclose(f); return 0; }
