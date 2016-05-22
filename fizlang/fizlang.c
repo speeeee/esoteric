ed644b4c12bb28455f1062c0cc6c0ddc0626f9db
Spencer <snsallay@gmail.com>
2016-05-21 23:08:43 -0400

    add basic handling of mouse presses as well as the mode/scene of the game

--- fizlang/fizlang.c
+++ fizlang/fizlang.c
@@ -9,9 +9,22 @@
 #include <OpenGL/GL.h>
 #include <GLFW/glfw3.h>
 
+#define SZ 1024
+
+#define BASE_HEIGHT 255
+#define SEA_LEVEL 127
+
+#define MTITLE 0
+#define MSETUP 1
+
+//int seed; int map[SZ][SZ];
+
 //typedef enum { Attract, Repel, Follow, Drift } Action;
 //typedef struct { int32_t v; int32_t type; /*Action act;*/ } Cell;
 //typedef struct { int32_t x; int32_t y; Cell *cls; } Tile;
+
+typedef struct { GLfloat x; GLfloat y; GLfloat w; GLfloat h; } Rect;
+typedef struct { Rect dim; Rect *but; } Window;
 
 void error_callback(int error, const char* description) {
     fputs(description, stderr); }
@@ -42,38 +55,48 @@ void rect(GLfloat x,GLfloat y,GLfloat w, GLfloat h) {
   glVertex3f(x,y,0); glVertex3f(x+w,y,0); glVertex3f(x+w,y+h,0); glVertex3f(x,y+h,0); }
 void tri(GLfloat x, GLfloat y, GLfloat lx, GLfloat ly) {
   glVertex3f(x,y,0); glVertex3f(x+lx,y,0); glVertex3f(x,y+ly,0); }
-void island(GLfloat x,GLfloat y,GLfloat w,GLfloat h) { glColor4f(0.8,0.95,0.8,1.0);
+/*void island(GLfloat x,GLfloat y,GLfloat w,GLfloat h) { glColor4f(0.8,0.95,0.8,1.0);
   glBegin(GL_QUADS); rect(x+0.1,y+0.1,w-0.2,h-0.2); glColor4f(0.6,0.75,0.6,1.0);
   rect(x,y+0.1,0.1,h-0.2); rect(x+0.1,y,w-0.2,0.1); glColor4f(0.55,0.7,0.55,1.0);
   rect(x+0.1,y+h-0.1,w-0.2,0.1); rect(x+w-0.1,y+0.1,0.1,h-0.2); glEnd();
   glBegin(GL_TRIANGLES); tri(x+0.1,y+0.1,-0.1,-0.1); glColor4f(0.45,0.6,0.45,1.0); 
   tri(x+w-0.1,y+0.1,0.1,-0.1); tri(x+0.1,y+h-0.1,-0.1,0.1);
-  tri(x+w-0.1,y+h-0.1,0.1,0.1); glEnd(); }
+  tri(x+w-0.1,y+h-0.1,0.1,0.1); glEnd(); }*/
+void ds_map(int sz,int bmap[sz][sz], int bh, int l, int r, int t, int b) {
+  int x_cnt = (r+l)/2; int y_cnt = (t+b)/2;
+  int cv = bmap[x_cnt][y_cnt] = (bmap[l][t]+bmap[r][t]+bmap[l][b]+bmap[r][b])/4
+    - (rand()%bh-bh/2)/2; //floor((double)rand()/RAND_MAX*bh);
+  bmap[x_cnt][t] = ((bmap[l][t]+bmap[r][t])-(rand()%bh-bh/2))/2;
+  bmap[x_cnt][b] = ((bmap[l][b]+bmap[r][b])-(rand()%bh-bh/2))/2;
+  bmap[l][y_cnt] = ((bmap[l][t]+bmap[l][b])-(rand()%bh-bh/2))/2;
+  bmap[r][y_cnt] = ((bmap[r][t]+bmap[r][b])-(rand()%bh-bh/2))/2;
+  if(r-l>2) { int nbh = ceil((double)bh*(double)pow(2.0,-0.75));
+    ds_map(sz,bmap,nbh,l,x_cnt,t,y_cnt); ds_map(sz,bmap,nbh,x_cnt,r,t,y_cnt);
+    ds_map(sz,bmap,nbh,l,x_cnt,y_cnt,b); ds_map(sz,bmap,nbh,x_cnt,r,y_cnt,b); } }
 
 void paint(GLFWwindow *win) { 
   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); glLoadIdentity();
   glClearColor(0.4,0.4,0.55,1.0);
   //glTranslatef(-c.x/10,-c.y/10,0); //printf("%f, %f, %f\n",c.z,c.x,c.y); 
   /*glBegin(GL_TRIANGLES); glVertex3f(0,0,0); glVertex3f(1,0,0);
-  glVertex3f(1,1,0); glVertex3f(0,1,0); glEnd();*/
-  island(0,0,1,1); }
+  glVertex3f(1,1,0); glVertex3f(0,1,0); glEnd();*/ }
 
 int pressed(GLFWwindow *win,int x) { return glfwGetKey(win,x)!=GLFW_RELEASE; }
+int mpressed(GLFWwindow *win, int x) { return glfwGetMouseButton(win,x); }
+// making drawing tool
+int bhover(GLFWwindow *win, Rect b) { Glfloat x, y;
+  glfwGetCursorPos(win,&x,&y); return x>b.x&&x<b.x+b.w&&y>b.y&&y<b.y+b.h; }
+int bclick(GLFWwindow *win, Rect b) { 
+  return mpressed(win,GLFW_MOUSE_BUTTON_LEFT)&&bhover(win,b); }
 
-/*cam getInput(GLFWwindow *win) { 
-  int l = -pressed(win,GLFW_KEY_LEFT); int r = pressed(win,GLFW_KEY_RIGHT);
-  int u = pressed(win,GLFW_KEY_UP); int d = -pressed(win,GLFW_KEY_DOWN);
-  int i = pressed(win,GLFW_KEY_I); int o = -pressed(win,GLFW_KEY_O);
-  return (cam) { l+r,u+d,i+o }; }*/
-
-
-/*cam parse_input(GLFWwindow *win, cam c) {
-  cam e = getInput(win); c = (cam){ c.x+e.x, c.y+e.y, e.z==0?c.z:1 };
-  if(e.z<0) { c = (cam){ 0, 0, 0 }; }
-  return c; }*/
-
+int parse_input(GLFWwindow *win, int mode) { Rect ng = { 30, 30, 100, 30 };
+  if(mpressed(win,ng)) { return MSETUP; } returm mode; }
+  
 int main(void) { //cam c = { 0, 0, 0 };
-  GLFWwindow* window; int seed = time(NULL); srand(seed); 
+  GLFWwindow* window; int seed = time(NULL); srand(seed); int mode = MTITLE;
+  /*map[0][0] = BASE_HEIGHT/2; map[SZ-1][0] = BASE_HEIGHT/2;
+  map[SZ-1][SZ-1] = BASE_HEIGHT/2; map[0][SZ-1] = BASE_HEIGHT/2;
+  ds_map(SZ,map,BASE_HEIGHT_A,0,SZ-1,SZ-1,0);*/
   glfwSetErrorCallback(error_callback);
   if (!glfwInit()) exit(EXIT_FAILURE);
   window = glfwCreateWindow(800, 800, "Fizlang", NULL, NULL);
@@ -85,7 +108,7 @@ int main(void) { //cam c = { 0, 0, 0 };
   glfwSetKeyCallback(window, key_callback); setup(window);
   glfwSetFramebufferSizeCallback(window, rsz);
   while (!glfwWindowShouldClose(window)) { paint(window);
-    /*c = parse_input(window,c);*/ glfwSwapBuffers(window); glfwPollEvents(); }
+    mode = parse_input(window,mode); glfwSwapBuffers(window); glfwPollEvents(); }
   glfwDestroyWindow(window);
   glfwTerminate();
   exit(EXIT_SUCCESS); }
